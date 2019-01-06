2f6858145166eed35948544283acc85adefe0ed5
Manuel Rabade <manuel@rabade.net>
2019-01-06 00:44:19 -0600

    configuramos registro de temperatura antes de lectura

--- firmware/termometro_tunete.ino
+++ firmware/termometro_tunete.ino
@@ -1,11 +1,13 @@
 const int TMP102_ADDRESS = 0x48;
+const int TMP102_REGISTER = 0x00;
 const int TMP102_BYTES = 2;
-const int TMP102_RETRY = 12;
-const int TMP102_DELAY = 250; // ms
+const int TMP102_RETRY = 10;
+const int TMP102_DELAY = 100; // ms
 
 double temp = -0xff;
 int err = 1;
 char buffer[8];
+int retry;
 
 const unsigned long SAMPLE_RATE = 30000; // ms
 unsigned long lastSample = 0;
@@ -22,6 +24,7 @@ int led = LOW;
 void setup() {
   Particle.variable("temperature", temp);
   Particle.variable("error", err);
+  Particle.variable("retry", retry);
   pinMode(D7, OUTPUT);
   Wire.begin();
 }
@@ -52,7 +55,10 @@ int update() {
     err = 2;
     return 0;
   }
-  for (int r = 0; r < TMP102_RETRY; r++) {
+  for (retry = 0; retry < TMP102_RETRY; retry++) {
+    Wire.beginTransmission(TMP102_ADDRESS);
+    Wire.write(TMP102_REGISTER);
+    Wire.endTransmission();
     Wire.requestFrom(TMP102_ADDRESS, TMP102_BYTES);
     if (Wire.available() == TMP102_BYTES) {
       byte MSB = Wire.read();
@@ -61,6 +67,7 @@ int update() {
       err = 0;
       return 1;
     } else {
+      Wire.reset();
       temp = -0xff;
       err = 3;
     }
