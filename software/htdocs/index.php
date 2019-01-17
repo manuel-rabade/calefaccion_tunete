d77d318e53aefd3863cf86a082b2964b6a1fb46d
Manuel Rabade <manuel@rabade.net>
2019-01-17 08:24:06 -0600

    simplificamos web

--- software/htdocs/index.php
+++ software/htdocs/index.php
@@ -1,6 +1,10 @@
+<!-- -*- mode: html; -*- -->
+
 <?php
 
 define('_ROOT_', dirname(dirname(dirname(__FILE__))));
+require(_ROOT_.'/software/lib/config.php');
+require(_ROOT_.'/software/lib/secrets.php');
 require(_ROOT_.'/software/lib/particle.php');
 require(_ROOT_.'/software/lib/openweathermap.php');
 
@@ -12,40 +16,49 @@ $owm = owmTemperature();
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossorigin="anonymous">
-    <!-- https://v4-alpha.getbootstrap.com/examples/narrow-jumbotron/ -->
-    <link href="css/narrow-jumbotron.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link href="css/tunete.css" rel="stylesheet">
     <title>Termometro Tunete</title>
   </head>
   <body>
     <div class="container">
-      <div class="header clearfix">
-        <nav>
-          <ul class="nav nav-pills float-right">
-            <li class="nav-item">
-              <a class="nav-link active"
-                 href="#">Uno <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Dos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tres</a>
-            </li>
-          </ul>
-        </nav>
-        <h3 class="text-muted">Termometro Tunete</h3>
-      </div>
-
       <div class="row">
-        <div class="col-sm-12">
-          <h4>Ahora</h4>
+        <div class="col-xs-12 col-md-8 section">
+          <h1>Sensores</h1>
+          <div class="row">
+            <div class="col-xs-6 col-md-3">
+              Uno
+            </div>
+            <div class="col-xs-6 col-md-3">
+              Dos
+            </div>
+            <div class="col-xs-6 col-md-3">
+              Tres
+            </div>
+            <div class="col-xs-6 col-md-3">
+              Cuatro
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-md-4">
+          <h1>Actuadores</h1>
+          <div class="row">
+            <div class="col-xs-6 col-md-6">
+              Uno
+            </div>
+            <div class="col-xs-6 col-md-6">
+              Dos
+            </div>
+          </div>
+        </div>
+        <hr/>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-md-8">
+          <h1>Debug</h1>
           <ul>
             <li>Temperatura Tunete: <?php echo $particle['temperature']; ?> &deg;C</li>
             <li>Temperatura Ambiente: <?php echo $owm['temperature']; ?> &deg;C</li>
@@ -53,32 +66,11 @@ $owm = owmTemperature();
         </div>
       </div>
     </div>
-
-    <!-- Optional JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
-
-<html>
-  <head>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
             integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-            crossorigin="anonymous"></script>
-    <link rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-          crossorigin="anonymous">
-  </head>
-  <body>
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   </body>
 </html>
